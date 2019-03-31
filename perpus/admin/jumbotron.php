d060a46260cff6cfc7198f8ee2084cc8d856158d
Nita Septiani <47415483+nitas12@users.noreply.github.com>
2019-03-31 15:13:18 +0700

    Add files via upload

---
+++ perpus/admin/jumbotron.php
@@ -0,0 +1,16 @@
+
+<body>
+<div class="container">
+<div class="jumbotron" style="background-color: rgba(255,255,255,0.60);">
+
+
+		<p><h1>Selamat Datang di eLibrary UBSI DWS </h1></p>
+		<p><h2>Sistem Library Universitas Bina Sarana Informatika Dewi Sartika!!!</p></h2>
+		<p><div class="glyphicon glyphicon-book" style="font-size: 42px;"></div></p>
+		<p><h4>Sistem ini merupakan sebuah halaman yang membantu dalam manajemen perpustakaan di UBSI DWS.</h4></p>
+		<p><h4>Dibuat dengan Kesederhanaan dan Penuh Semangat </h4></p>
+		<p><a class="btn btn-primary btn-lg" role="button" href="?page=buku">Lanjutkan . . .</a></p>
+	</div>
+</div>
+</body>
+</html>
